ef470aba98fefc2a338420b18067befd4dc8bf0b
prithvi41 <rajp7163@gmail.com>
2021-08-20 10:56:46 +0530

    Update tut01.py

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,5 +1,29 @@
 def meraki_helper(n):
-    """This will detect meraki numner"""
+    count=0
+    for i in n:
+        if(i<11):
+            print("Yes - ",i ,"is a Meraki number")
+            count+=1
+            
+        else:
+            j=i
+            flag=True
+            while i>10:
+                k=i%10
+                i=i//10
+                l=i%10
+                if(abs(k-l)!=1):
+                    print("NO - ",j ,"is not a Meraki number")
+                    flag=False
+                    break
+            if(flag):
+                print("Yes - ",j ,"is a Meraki number")
+                count+=1
+    print("the input list contains",count," meraki and", len(n)-count," non meraki numbers")
+    
+meraki_helper(n)
 
 
-input = [12, 14, 56, 78, 98, 54, 678, 134, 789, 0, 7, 5, 123, 45, 76345, 987654321]
+
+
+#input = [12, 14, 56, 78, 98, 54, 678, 134, 789, 0, 7, 5, 123, 45, 76345, 987654321]
